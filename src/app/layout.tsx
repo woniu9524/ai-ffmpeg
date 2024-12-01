9edcc1fdc8537a3a29f92caaa6219009e24e2252
zhangcheng <952490637@qq.com>
2024-12-01 11:39:27 +0800

    refactor(app): 替换 Google Tag Manager 为 Google Analytics
    - 移除 GoogleTagManager 组件，使用 GoogleAnalytics 组件替代- 更新 package.json，添加 @next/third-parties 依赖
    - 更新 pnpm-lock.yaml，添加 @next/third-parties 和 third-party-capital 依赖

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import '@/styles/globals.css';
 import { Metadata } from 'next';
 import { Toaster } from 'sonner';
 import '@/i18n';  // 导入 i18n 配置
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({
   children,
@@ -16,7 +16,7 @@ export default function RootLayout({
       <body>
         <Toaster richColors />
         {children}
-        <GoogleTagManager gtmId="G-YYZZQP968S" />
+        <GoogleAnalytics gaId="G-YYZZQP968S" />
       </body>
     </html>
   );
